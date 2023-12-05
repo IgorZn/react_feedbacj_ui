a33e29ec8006b419ca7d5ef58d27ca83798936c2
igor.znamensky <igor.znamensky@gmail.com>
2023-12-05 09:48:58 +0300

    Rating Select Component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,25 @@
 import React, {useState} from 'react';
 import Card from './shared/Card'
 import Button from "./shared/Button";
+import {isDisabled} from "@testing-library/user-event/dist/utils";
 
 function FeedbackForm(props) {
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
+
+
     const handleTextChange = (e) => {
+        if(text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if(text !== '' && text.trim().length <= 10 ) {
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value)
     }
 
@@ -18,8 +33,9 @@ function FeedbackForm(props) {
                     type="text"
                     value={text}
                     placeholder={"Write a review"}/>
-                <Button type={"submit"}>Sent</Button>
+                <Button type={"submit"} isDisabled={btnDisabled}>Sent</Button>
             </div>
+            {message && <div className="message">{message}</div>}
         </form>
     </Card>
 }
