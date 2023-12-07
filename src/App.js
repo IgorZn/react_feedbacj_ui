3f817fa76bb492a5448e08d8decfc0964f124759
igor.znamensky <igor.znamensky@gmail.com>
2023-12-07 09:39:12 +0300

    5 Creating Links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
+import {useState} from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+//
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
 import FeedbackData from "./fakeData";
 import FeedBackList from "./components/FeedBackList";
-import {useState} from "react";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import About from "./pages/About";
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
 
     return (
         <>
-            <Header/>
-            <div className="container">
-                <FeedbackForm setFeedback={setFeedback}/>
-                <FeedbackStats feedback={feedback} />
-                <FeedBackList feedback={feedback} setFeedback={setFeedback}/>
-            </div>
+            <BrowserRouter>
+                <Header/>
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/" element={
+                            <>
+                                <FeedbackForm setFeedback={setFeedback}/>
+                                <FeedbackStats feedback={feedback}/>
+                                <FeedBackList feedback={feedback} setFeedback={setFeedback}/>
+                            </>
+                        }>
+                        </Route>
+                        <Route path="/about" element={<About/>}/>
+                    </Routes>
+                </div>
+            </BrowserRouter>
+
         </>
     );
 }
-
 
 
 export default App;
